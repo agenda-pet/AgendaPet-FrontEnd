373fd5580ffda36ec4267572b93aa57845c25e55
pedroacg80 <pedroacg80@gmail.com>
2026-06-11 10:27:38 -0300

    :sparkles: Componente Header

--- agenda-pet/src/components/header/header.tsx
+++ agenda-pet/src/components/header/header.tsx
@@ -1,10 +1,28 @@
-
+import styles from "./header.module.css"
 
 const Header = () => {
     return (
-    <>
-    </>
+        <header className={styles.header}>
+            <div className={styles.container}>
+                <img src="../imgs/LogoAgendaPet.png" alt="Logo Agenda Pet" id={styles.Logo} />
+                <nav className={styles.nav_bar}>
+                    <a href="">Agendamento</a>
+                    <a href="">Historico</a>
+                    <a href="">Pets</a>
+                    <a href="">Usuarios</a>
+                </nav>
+                <div id={styles.info_header}>
+                    <div id={styles.icone}>
+                        <img src="../imgs/IconeUsuario.png" alt="" />
+                    </div>
+                    <div id={styles.usuario_header}>
+                        <h1>Felipea</h1>
+                        <h2>felipe@gmail.com</h2>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
-export default Header
+export default Header;
