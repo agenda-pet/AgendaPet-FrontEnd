60ab10e75effa27834feafc1248d4ef1e089190d
Pablo Henrique Pedroza da Silva <48558840814@info.local>
2026-06-17 09:59:47 -0300

    detalheAgendamento feito

--- agenda-pet/src/components/card/card.tsx
+++ agenda-pet/src/components/card/card.tsx
@@ -51,6 +51,45 @@ const Card = ({ page }: CardProps) => {
                     <td><img src="../imgs/IconePatinhaInformacoes.png" alt="" /></td>
                 </tr>
             )}
+
+            {page === "detalheHistoricoItem" && (
+                <div className={styles.cardHistoricoContainer}>
+                    <table className={styles.tabelaInternaHistorico}>
+                        <thead>
+                            <tr>
+                                <th>Data</th>
+                                <th>Pet</th>
+                                <th>Horário</th>
+                                <th>Tutor</th>
+                                <th>Status</th>
+                                <th>Serviços</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>09/02/2026</td>
+                                <td>Belinha</td>
+                                <td>17:30</td>
+                                <td>Allan</td>
+                                <td>
+                                    <span className={styles.statusPendente}>Pendente</span>
+                                </td>
+                                <td>
+                                    <select className={styles.selectServicos} defaultValue="Banho">
+                                        <option value="Banho">Banho</option>
+                                        <option value="tosa">tosa</option>
+                                        <option value="unha">Cortar unha</option>
+                                    </select>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                    <div className={styles.dataAlteracaoContainer}>
+                        <span>Data Alteração: 10/02/2026</span>
+                    </div>
+                </div>
+            )}
         </>
     )
 }
--- agenda-pet/src/components/lista/lista.tsx
+++ agenda-pet/src/components/lista/lista.tsx
@@ -9,7 +9,6 @@ const Lista = ({ page }: ListaProps) => {
     return (
         <>
             {page === "listaAgendamento" && (
-
                 <section className={styles.section}>
                     <table className={styles.tabelaLista}>
                         <thead id={styles.thead}>
@@ -23,20 +22,19 @@ const Lista = ({ page }: ListaProps) => {
                                 <th>Editar</th>
                             </tr>
                         </thead>
-
                         <tbody id={styles.tbody}>
-                            <Card page="listaAgendamento"/>
-                            <Card page="listaAgendamento"/>
-                            <Card page="listaAgendamento"/>
-                            <Card page="listaAgendamento"/>
-                            <Card page="listaAgendamento"/>
+                            <Card page="listaAgendamento" />
+                            <Card page="listaAgendamento" />
+                            <Card page="listaAgendamento" />
+                            <Card page="listaAgendamento" />
+                            <Card page="listaAgendamento" />
                         </tbody>
                     </table>
                 </section>
             )}
 
             {page === "listaPets" && (
-                  <section className={styles.section}>
+                <section className={styles.section}>
                     <table className={styles.tabelaLista}>
                         <thead id={styles.thead}>
                             <tr>
@@ -50,18 +48,18 @@ const Lista = ({ page }: ListaProps) => {
                         </thead>
 
                         <tbody id={styles.tbody}>
-                            <Card page="listaPets"/>
-                            <Card page="listaPets"/>
-                            <Card page="listaPets"/>
-                            <Card page="listaPets"/>
-                            <Card page="listaPets"/>
+                            <Card page="listaPets" />
+                            <Card page="listaPets" />
+                            <Card page="listaPets" />
+                            <Card page="listaPets" />
+                            <Card page="listaPets" />
                         </tbody>
                     </table>
                 </section>
             )}
 
             {page === "listaUsuarios" && (
-                  <section className={styles.section}>
+                <section className={styles.section}>
                     <table className={styles.tabelaLista}>
                         <thead id={styles.thead}>
                             <tr>
@@ -74,15 +72,86 @@ const Lista = ({ page }: ListaProps) => {
                         </thead>
 
                         <tbody id={styles.tbody}>
-                            <Card page="listaUsuarios"/>
-                            <Card page="listaUsuarios"/>
-                            <Card page="listaUsuarios"/>
-                            <Card page="listaUsuarios"/>
-                            <Card page="listaUsuarios"/>
+                            <Card page="listaUsuarios" />
+                            <Card page="listaUsuarios" />
+                            <Card page="listaUsuarios" />
+                            <Card page="listaUsuarios" />
+                            <Card page="listaUsuarios" />
                         </tbody>
                     </table>
                 </section>
             )}
+
+            {page === "listaHistorico" && (
+                <section className={styles.section}>
+                    <table className={styles.tabelaLista}>
+                        <thead id={styles.thead}>
+                            <tr>
+                                <th>Data</th>
+                                <th>Horário</th>
+                                <th>Raça</th>
+                                <th>Porte</th>
+                                <th>Pets</th>
+                                <th>Serviços</th>
+                                <th>Editar</th>
+                            </tr>
+                        </thead>
+                        <tbody id={styles.tbody}>
+                            <Card page="listaAgendamento" />
+                            <Card page="listaAgendamento" />
+                            <Card page="listaAgendamento" />
+                            <Card page="listaAgendamento" />
+                            <Card page="listaAgendamento" />
+                        </tbody>
+                    </table>
+                </section>
+            )}
+
+            {page === "listaDetalheAgendamento" && (
+                <section className={styles.containerDetalhes}>
+                    {/* Botão de Voltar */}
+                    <div className={styles.voltarContainer}>
+                        <span className={styles.btnVoltar}>&larr; Voltar</span>
+                    </div>
+
+                    <h2 className={styles.tituloSecao}>Agendamentos: (Nome do Cliente)</h2>
+
+                    <div className={styles.cardAgendamentoAtual}>
+                        <div className={styles.infoGroup}>
+                            <span className={styles.label}>Cliente</span>
+                            <span className={styles.valor}>Allan</span>
+                        </div>
+                        <div className={styles.infoGroup}>
+                            <span className={styles.label}>Porte</span>
+                            <span className={styles.valor}>Pequeno</span>
+                        </div>
+                        <div className={styles.infoGroup}>
+                            <span className={styles.label}>Pet</span>
+                            <span className={styles.valor}>Pingu</span>
+                        </div>
+                        <div className={styles.infoGroup}>
+                            <span className={styles.label}>Data de Agendamento</span>
+                            <span className={styles.valor}>09/02/2026</span>
+                        </div>
+                        <div className={styles.infoGroup}>
+                            <span className={styles.label}>Horário</span>
+                            <span className={styles.valor}>17:30</span>
+                        </div>
+                        <div className={styles.infoGroup}>
+                            <span className={styles.label}>Valor</span>
+                            <span className={styles.valor}>R$ 30.00</span>
+                        </div>
+                    </div>
+
+                    <h3 className={styles.subTituloSecao}>Histórico:</h3>
+
+                    <div className={styles.historicoLista}>
+                        <Card page="detalheHistoricoItem" />
+                        <Card page="detalheHistoricoItem" />
+                    </div>
+                </section>
+            )}
+
         </>
     )
 }
