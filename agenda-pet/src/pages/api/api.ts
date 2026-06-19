6cade085e3316c70bd353b8b75269cdf087fbb69
Queiroz754 <allan754@outlook.com>
2026-06-19 08:13:19 -0300

    :twisted_rightwards_arrows: Merge com Agendamento

--- agenda-pet/src/pages/api/api.ts
+++ agenda-pet/src/pages/api/api.ts
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import secureLocalStorage from "react-secure-storage";
 import axios from "axios";
 
@@ -21,42 +19,4 @@ api.interceptors.request.use((config) => {
 
   return config;
 });
-=======
-import axios from "axios";
-import secureLocalStorage from "react-secure-storage";
 
-=======
-import axios from "axios";
-
->>>>>>> feature/listaPets
-=======
-import axios from "axios";
-
->>>>>>> feature/listaAgendamentos
-const apiLocal = " https://localhost:7057/api/";
-
-const apiRemota = "";
-
-// criar um endereco da API dentro do axios
-export const api = axios.create({
-    baseURL: apiLocal
-<<<<<<< HEAD
-})
-
-// api.interceptors.request.use((config) => {
-//     const token = secureLocalStorage.getItem("Token");
-
-//     if(token){
-//         config.headers.Authorization = "Bearer " + token;
-//     }
-
-//     return config;
-<<<<<<< HEAD
-// });
->>>>>>> feature/listaUsuarios
-=======
-// });
->>>>>>> feature/listaPets
-=======
-})
->>>>>>> feature/listaAgendamentos
